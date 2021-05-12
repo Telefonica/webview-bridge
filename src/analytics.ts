8b9610857ec71c69b2d0d9aab9c96350a3c2c35e
yamal <yamal@tuenti.com>
2021-05-12 10:48:23 +0200

    WEB-188 Fix style

--- src/analytics.ts
+++ src/analytics.ts
@@ -366,4 +366,6 @@ export const setTrackingProperty = (
             name: name,
             value: value,
         },
-    }).catch(() => {});
+    }).catch(() => {
+        // do nothing
+    });

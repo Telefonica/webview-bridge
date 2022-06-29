b16061f409437906583e2795b4e1871ac3ecd44c
Pedro <pedro.jose.ladaria.linden@telefonica.com>
2022-06-29 16:43:12 +0200

    WEB-595 update comment

--- src/message-id.ts
+++ src/message-id.ts
@@ -4,7 +4,7 @@ const webviewId = `${Date.now()}-${String(Math.random()).slice(-8)}`;
 /**
  * Message ID generator. Ids should be unique.
  *
- * the "web" prefix indicates that the was originated from the web side.
+ * the "web" prefix indicates that the message was originated from the web side.
  *
  * Using a timestamp as webviewId (assuming two webviews are not opened in the same millisecond),
  * but if that ever happens, the last part is a random number to avoid collisions.

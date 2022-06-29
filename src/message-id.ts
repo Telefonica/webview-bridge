4c0be426bdb982b99b5317e29fc743913909a2b4
Pedro <pedro.jose.ladaria.linden@telefonica.com>
2022-06-29 11:24:13 +0200

    WEB-595 unique bridge ids

--- src/message-id.ts
+++ src/message-id.ts
@@ -1,3 +1,13 @@
-let id = 1;
+let messageId = 1;
+const webviewId = Date.now();
 
-export const getId = (): string => `webapp-${id++}`;
+/**
+ * Message ID generator. Ids should be unique.
+ *
+ * the "web" prefix indicates that the was originated from the web side.
+ *
+ * Using a timestamp as webviewId (assuming two webviews are not opened in the same millisecond),
+ * but if that ever happens, the last part is a random number to avoid collisions.
+ */
+export const getId = (): string =>
+    `web-${messageId++}-${webviewId}-${String(Math.random()).slice(-8)}`;

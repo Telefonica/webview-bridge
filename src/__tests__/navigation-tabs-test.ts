6991f4bd9480fc8e45eeec334dc55cff889b7cf9
Josefina <josefina.maria.perez.cruz-aunon@telefonica.com>
2025-01-23 19:04:21 +0100

    B2PDE-1403 Add tests for refreshNavBar

---
+++ src/__tests__/navigation-tabs-test.ts
@@ -0,0 +1,52 @@
+import {refreshNavBar} from '../navigation-tabs';
+import {
+    createFakeAndroidPostMessage,
+    removeFakeAndroidPostMessage,
+} from './fake-post-message';
+
+const MODULE_ID = 'bottombar';
+const PRODUCT_ID = 'myProductId';
+
+afterEach(() => {
+    removeFakeAndroidPostMessage();
+});
+
+test('refresh navigation bars without params', (done) => {
+    createFakeAndroidPostMessage({
+        checkMessage: (message) => {
+            expect(message.type).toBe('REFRESH_NAV_BAR');
+        },
+        getResponse: (message) => ({
+            type: message.type,
+            id: message.id,
+        }),
+    });
+
+    refreshNavBar({}).then((res) => {
+        expect(res).toBeUndefined();
+        done();
+    });
+});
+
+test('refresh navigation bars with params', (done) => {
+    createFakeAndroidPostMessage({
+        checkMessage: (message) => {
+            expect(message.type).toBe('REFRESH_NAV_BAR');
+            expect(message.payload).toEqual({
+                module_id: MODULE_ID,
+                product_id: PRODUCT_ID,
+            });
+        },
+        getResponse: (message) => ({
+            type: message.type,
+            id: message.id,
+        }),
+    });
+
+    refreshNavBar({module_id: MODULE_ID, product_id: PRODUCT_ID}).then(
+        (res) => {
+            expect(res).toBeUndefined();
+            done();
+        },
+    );
+});

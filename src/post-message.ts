1f72fcc93ba10273e04422de9491bb5bce3f7357
Jesús Latorre <jesus.latorreasensio@telefonica.com>
2026-03-27 10:33:07 +0100

    Fix issues

--- src/post-message.ts
+++ src/post-message.ts
@@ -1,5 +1,4 @@
 import {getId} from './message-id';
-import type {LocatorSdkConfig} from './family-locator';
 
 /**
  * There are two possible kind of messages we can receive from native app:
@@ -544,7 +543,7 @@ export type ResponsesFromNativeApp = {
     GET_LOCATOR_SDK_CONFIG: {
         type: 'GET_LOCATOR_SDK_CONFIG';
         id: string;
-        payload: {config: LocatorSdkConfig | null};
+        payload: {config: Record<string, unknown> | null};
     };
 
     REQUEST_ALLOWME_BIOMETRICS: {
